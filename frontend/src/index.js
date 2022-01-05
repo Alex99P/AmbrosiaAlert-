67091f2cb8934d7ce33e78340793e99b6c2efcbe
Alexandru Patroi <alexandru.patroi@opswat.com>
2022-01-05 23:53:54 +0200

    add server folder

---
+++ frontend/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import NavBar from './components/navBar';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Row, Col,Container } from 'react-bootstrap';
+
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+   {/* <Container>
+  <Row>
+    <Col ><NavBar/></Col>
+    <Col sm={12}><App /></Col>
+  </Row>
+  </Container> */}
+
+  <NavBar/>
+  <App/>
+  
+
+  
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
